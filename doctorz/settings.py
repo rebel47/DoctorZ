eef32b0af809a8a2543eac3da1a62ac32f5196a1
Ayaz <aayyaazz123@gmail.com>
2021-06-06 16:27:44 +0530

    heroku

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from django.contrib import messages
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,7 @@ EMAIL_HOST_PASSWORD = 'Jugaad@Logic786'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+#  Activate Django-Heroku
+django_heroku.settings(locals())
