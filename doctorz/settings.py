d3dcbcd679bb94e2492387e612f6923c7b35d9a6
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2021-06-10 01:45:48 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -132,8 +132,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "logickiddie@gmail.com"
-EMAIL_HOST_PASSWORD = 'Jugaad@Logic786'
+EMAIL_HOST_USER = "email"
+EMAIL_HOST_PASSWORD = 'password'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
