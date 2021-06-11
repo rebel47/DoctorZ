3495faef398b2dd062114a53fb25612381b4dfdf
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2021-06-12 00:40:40 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -138,7 +138,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "logickiddie@gmail.com"
-EMAIL_HOST_PASSWORD = 'Jugaad@Logic786'
+EMAIL_HOST_PASSWORD = ''
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
