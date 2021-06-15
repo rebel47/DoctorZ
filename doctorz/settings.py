33ecb79f48053cc89abfff3b02eb4dd55687a2e0
Ayaz <aayyaazz123@gmail.com>
2021-06-15 16:23:32 +0530

    make it better

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -4,7 +4,7 @@ import os
 from django.contrib import messages
 import django_heroku
 import dj_database_url
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
