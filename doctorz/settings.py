5f769e103e63eeaf7a5061ca23ebdad2b18f0f79
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2021-06-15 16:37:47 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -115,6 +115,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "/static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
