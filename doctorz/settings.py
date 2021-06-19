73d4a40ebfa42ad2c5a8b82b4c490162d9dae7e1
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2021-06-19 17:10:57 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # TEMPLATES_ROOT = os.path.join(BASE_DIR, 'templates')
 
 
