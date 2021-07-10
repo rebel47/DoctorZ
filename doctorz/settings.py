6d01c948ddf957ce49d31128ca11087dc32ddb23
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2021-07-10 16:59:47 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATIC_URL = '/static/'
