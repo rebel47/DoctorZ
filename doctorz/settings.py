041631a1550c8cba70ef31530cd3b8783ac3c531
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2021-07-10 18:22:46 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -128,7 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # )
 # TEMPLATES_ROOT = os.path.join(BASE_DIR, 'templates')
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
