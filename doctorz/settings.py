3b4fa9822d340f7148b838dde9c3af2789d404f4
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2021-07-10 16:52:58 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -116,12 +116,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-   ('doctorz', os.path.join(BASE_DIR, 'doctorz', 'static')),
-)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#    ('doctorz', os.path.join(BASE_DIR, 'doctorz', 'static')),
+# )
 STATICFILES_FINDERS = (
   'django.contrib.staticfiles.finders.FileSystemFinder',
   'django.contrib.staticfiles.finders.AppDirectoriesFinder',
