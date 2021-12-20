32348f71778f53d5bf00a3dc26f7bca7590cc806
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2021-12-20 22:45:53 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -144,11 +144,11 @@ STATICFILES_FINDERS = (
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.rediffmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "logickiddie@gmail.com"
-EMAIL_HOST_PASSWORD = 'Password'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = "aayyaazz@rediffmail.com"
+EMAIL_HOST_PASSWORD = 'ayaz786786'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
