b8987282bbb1ed0eebe5a261d1265dfd5edd6b22
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2022-04-08 08:23:11 +0530

    Update settings.py

--- doctorz/settings.py
+++ doctorz/settings.py
@@ -148,9 +148,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "logickiddie@gmail.com"
-EMAIL_HOST_PASSWORD = ''
-# Passoword is saved in sticky file
-# Make sure to activate less secure app option in gmail
+EMAIL_HOST_PASSWORD = 'Hallo@786@'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
